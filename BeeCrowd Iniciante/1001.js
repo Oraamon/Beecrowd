f15514ed5de6817ffff908d1ad3cfd07617f23aa
Horácio Linhares Monte <98846292+Oraamon@users.noreply.github.com>
2022-04-27 13:05:08 -0300

    Create 1001.js

---
+++ BeeCrowd Iniciante/1001.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var prompt = function(texto) { return lines.shift();};
+var x = parseInt(prompt("x"));
+var y= parseInt(prompt("y"));
+
+var resultado = x + y
+
+
+console.log ("X = " + resultado);
