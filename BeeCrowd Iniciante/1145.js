bc13387a75a609ac634ea8671465d18a3b6950fc
Horácio Linhares Monte <98846292+Oraamon@users.noreply.github.com>
2022-05-16 09:40:55 -0300

    Create 1145.js

---
+++ BeeCrowd Iniciante/1145.js
@@ -0,0 +1,14 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var z = parseInt(lines.shift())
+var [a,b,c] = []
+for(x=1;x<=z;x++){
+    b= x*x
+    a =x
+    c = x*x*x
+    console.log(a,b,c)
+    
+    console.log(a,b+1,c+1)
+    
+   
+}
